b281f3784183a2543a3cba663ad06c7fb3fcab99
mintu720 <86738135+mintu720@users.noreply.github.com>
2022-06-15 00:06:29 +0530

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -11,8 +11,8 @@ class UtubeBot(Client):
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             plugins=dict(root="bot.plugins"),
-            workers=6,
+            workers=9898,
         )
-        self.DOWNLOAD_WORKERS = 6
+        self.DOWNLOAD_WORKERS = 9898
         self.counter = 0
         self.download_controller = {}
